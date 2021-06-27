3ad7d5f0c3bb7d6f3918b738373f0587e0b41119
Lucas C. Noman <lucas.noman7@gmail.com>
2021-06-27 11:49:58 -0300

    fix: added correct verification of admin user for tags posting

--- src/middleware/ensureAdmin.ts
+++ src/middleware/ensureAdmin.ts
@@ -1,13 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories';
 
-export function ensureAdmin(
+export async function ensureAdmin(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
   const { user_id } = request;
 
-  const admin = true;
+  const usersRepositories = getCustomRepository(UsersRepositories);
+
+  const { admin } = await usersRepositories.findOne(user_id);
 
   if (admin) {
     return next();
