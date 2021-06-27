0aef240712341affd059c0215ef919d508d20342
Lucas C. Noman <lucas.noman7@gmail.com>
2021-06-27 12:16:32 -0300

    refactor: just added more info to compliments listing

--- src/services/ListUserReceiveComplimentsService.ts
+++ src/services/ListUserReceiveComplimentsService.ts
@@ -11,6 +11,7 @@ export class ListUserReceiveComplimentsService {
       where: {
         user_receiver: user_id,
       },
+      relations: ['userSender', 'userReceiver', 'tag'],
     });
 
     return compliments;
